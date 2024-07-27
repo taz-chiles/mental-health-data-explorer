f344204090530ac11336055761ff0e24749069eb
Taz <97239140+taz-chiles@users.noreply.github.com>
2024-07-27 12:01:57 +0100

    Add first nhs data

--- _config.ts
+++ _config.ts
@@ -4,10 +4,29 @@ import date from "lume/plugins/date.ts";
 import metas from "lume/plugins/metas.ts";
 import postcss from "lume/plugins/postcss.ts";
 
+// Importing the OI Lume charts and utilities
+import oiViz from "https://deno.land/x/oi_lume_viz@v0.15.1/mod.ts";
+import autoDependency from "https://deno.land/x/oi_lume_utils@v0.4.0/processors/auto-dependency.ts";
+import csvLoader from "https://deno.land/x/oi_lume_utils@v0.4.0/loaders/csv-loader.ts";
+import jsonLoader from "lume/core/loaders/json.ts";
+
 const site = lume({
   src: './src',
   location: new URL("https://taz-chiles.github.io/mental-health-data-explorer/"),
 });
+
+site.loadData([".csv", ".tsv", ".dat"], csvLoader({ basic: true }));
+site.loadData([".geojson"], jsonLoader);
+site.loadData([".hexjson"], jsonLoader);
+
+// Import lume viz
+import oiVizConfig from "./oi-viz-config.ts";
+site.use(oiViz(oiVizConfig));
+
+site.use(oiViz({
+	assetPath: '/assets/oi',
+	componentNamespace: 'oi.viz'
+}));
 
 site.use(base_path());
 site.use(metas({
