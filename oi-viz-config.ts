be43445e917cfe1beb4c36d67ef48e6f6c369072
Taz <97239140+taz-chiles@users.noreply.github.com>
2024-07-28 17:39:41 +0100

    Messing around with colours

--- oi-viz-config.ts
+++ oi-viz-config.ts
@@ -6,6 +6,10 @@ export default {
 		"weight": 'normal',
 		"size": 16
 	},
+	"colour": {
+		"series": 
+			[ '#97d4ee','#76e994','#3ba4b8','#c98cca','#e13cfa','#29af7f', '#a67cf3']
+	},
 	"map": {
 		"tileLayer": 'CartoDB.Positron'
 	}
